f90366dbbcc63552165c0b1cc8e7fb25936c452b
SanchelloS <Sanchellos577@gmail.com>
2025-03-17 09:55:15 +0300

    Создан класс

--- PR1/Program.cs
+++ PR1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Course { get; set; }
+    public bool IsMale { get; set; }
+
+  
+    public Student(string name, int course, bool isMale)
+    {
+        Name = name;
+        Course = course;
+        IsMale = isMale;
+    }
+
+ 
+    public void DisplayInfo()
+    {
+        string gender = IsMale ? "Мужской" : "Женский";
+        Console.WriteLine($"Имя: {Name}, Курс: {Course}, Пол: {gender}");
+    }
+}
